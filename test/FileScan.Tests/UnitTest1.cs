3ecbcd95d0d0c910b762739ad2f71ff07d67dafd
acn-sbuad <stephanie.buadu@avanade.com>
2023-01-18 17:36:08 +0100

    added stylecop

--- test/FileScan.Tests/UnitTest1.cs
+++ test/FileScan.Tests/UnitTest1.cs
@@ -2,8 +2,14 @@ using Xunit;
 
 namespace FileScan.Tests
 {
+    /// <summary>
+    /// Unit test class
+    /// </summary>
     public class UnitTest1
     {
+        /// <summary>
+        /// First test method
+        /// </summary>
         [Fact]
         public void TestMethod1()
         {
