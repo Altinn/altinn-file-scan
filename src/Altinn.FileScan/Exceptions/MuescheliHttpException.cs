8099472c26cbc56cd2c595e65063ec3e914d546b
Terje Holene <terje.holene@digdir.no>
2024-09-26 08:40:18 +0200

    Bump github codeql-action autobuild (#186)
    * Bump github codeql-action autobuild
    * fix: Remove serializable from custom exceptions

--- src/Altinn.FileScan/Exceptions/MuescheliHttpException.cs
+++ src/Altinn.FileScan/Exceptions/MuescheliHttpException.cs
@@ -1,43 +1,33 @@
 ﻿using System.Net;
-using System.Runtime.Serialization;
 
-namespace Altinn.FileScan.Exceptions
+namespace Altinn.FileScan.Exceptions;
+
+/// <summary>
+/// An exception class related to non expected http response from the Muescheli Service
+/// </summary>
+public class MuescheliHttpException : Exception
 {
     /// <summary>
-    /// An exception class related to non expected http response from the Muescheli Service
+    /// The http response message that generated the exception
     /// </summary>
-    [Serializable]
-    public class MuescheliHttpException : Exception
+    public HttpResponseMessage Response { get; }
+
+    /// <summary>
+    /// Creates a new <see cref="MuescheliHttpException"/> combining the response message and 
+    /// </summary>
+    public static async Task<MuescheliHttpException> CreateAsync(HttpStatusCode statusCode, HttpResponseMessage response)
     {
-        /// <summary>
-        /// The http response message that generated the exception
-        /// </summary>
-        public HttpResponseMessage Response { get; }
+        string responseMessage = await response.Content.ReadAsStringAsync();
 
-        /// <summary>
-        /// Creates a new <see cref="MuescheliHttpException"/> combining the response message and 
-        /// </summary>
-        public static async Task<MuescheliHttpException> CreateAsync(HttpStatusCode statusCode, HttpResponseMessage response)
-        {
-            string responseMessage = await response.Content.ReadAsStringAsync();
+        string message = $"{statusCode} - {responseMessage}";
+        return new MuescheliHttpException(response, message);
+    }
 
-            string message = $"{statusCode} - {responseMessage}";
-            return new MuescheliHttpException(response, message);
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MuescheliHttpException"/> class.
-        /// </summary>
-        public MuescheliHttpException(HttpResponseMessage response, string message) : base(message)
-        {
-            Response = response;
-        }
-
-        /// <summary>
-        /// Add serialization info.
-        /// </summary>
-        protected MuescheliHttpException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
-        }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MuescheliHttpException"/> class.
+    /// </summary>
+    public MuescheliHttpException(HttpResponseMessage response, string message) : base(message)
+    {
+        Response = response;
     }
 }
