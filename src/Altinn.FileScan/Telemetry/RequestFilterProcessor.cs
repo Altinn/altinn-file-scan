c6e0cfcbe954a451bd3eed9d6ebba7ff6e5f3fba
Sigrid Bratsberg <sigrid.bratsberg@digdir.no>
2025-03-04 10:54:17 +0100

    Consolidate OTEL setup to be similar to Storage

---
+++ src/Altinn.FileScan/Telemetry/RequestFilterProcessor.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Primitives;
+using OpenTelemetry;
+
+namespace Altinn.FileScan.Telemetry
+{
+    /// <summary>
+    /// Filter for requests (and child dependencies) that should not be logged.
+    /// </summary>
+    public class RequestFilterProcessor : BaseProcessor<Activity>
+    {
+        private const string RequestKind = "Microsoft.AspNetCore.Hosting.HttpRequestIn";
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RequestFilterProcessor"/> class.
+        /// </summary>
+        public RequestFilterProcessor(IHttpContextAccessor httpContextAccessor = null) : base()
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        /// <summary>
+        /// Determine whether to skip a request
+        /// </summary>
+        public override void OnStart(Activity activity)
+        {
+            bool skip = false;
+            if (activity.OperationName == RequestKind)
+            {
+                skip = ExcludeRequest(_httpContextAccessor.HttpContext.Request.Path.Value);
+            }
+            else if (!(activity.Parent?.ActivityTraceFlags.HasFlag(ActivityTraceFlags.Recorded) ?? true))
+            {
+                skip = true;
+            }
+
+            if (skip)
+            {
+                activity.ActivityTraceFlags &= ~ActivityTraceFlags.Recorded;
+            }
+        }
+
+        /// <summary>
+        /// No action on end
+        /// </summary>
+        /// <param name="activity">xx</param>
+        public override void OnEnd(Activity activity)
+        {
+            if (activity.OperationName == RequestKind && _httpContextAccessor.HttpContext is not null)
+            {
+                if (_httpContextAccessor.HttpContext.Request.Headers.TryGetValue("X-Forwarded-For", out StringValues ipAddress))
+                {
+                    activity.SetTag("ipAddress", ipAddress.FirstOrDefault());
+                }
+            }
+        }
+
+        private bool ExcludeRequest(string localpath)
+        {
+            return localpath switch
+            {
+                var path when path.TrimEnd('/').EndsWith("/health", StringComparison.OrdinalIgnoreCase) => true,
+                _ => false
+            };
+        }
+    }
+}
